1ce7fc0adac68dd6fb8f96f1af12e3f3e904a444
Kaushik Thallapally <>
2020-12-26 20:13:17 -0800

    Initial Commit Ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import amplify from "./amplify.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            display: "flex",
+            // justifyContent: "space-between",
+            alignContent: "center",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <span style={{ fontSize: "10em" }}>+</span>
+          <img
+            src={amplify}
+            className="App-logo"
+            alt="logo"
+            style={{ paddingLeft: "5rem" }}
+          />
+        </div>
+        <h2>Demo of React App deployment into AWS Amplify</h2>
+        <p>
+          <a
+            className="App-link"
+            href="https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            AWS Amplify
+          </a>
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
       </header>
     </div>
   );
